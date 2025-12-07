d52bc0449ad2a1f840398ab4ad8d73b684abc85f
EducaDUG <duguevara@gmail.com>
2025-12-07 14:35:46 +1300

    Update ask.js

--- api/ask.js
+++ api/ask.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
     }
 
     const url =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" +
       apiKey;
 
     const geminiResponse = await fetch(url, {
